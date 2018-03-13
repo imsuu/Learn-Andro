3b9e0f673cea5c6d25794eccd559d6d4d327bb1d
imsuu <943707709@qq.com>
2018-03-13 17:44:59 +0800

    活动

--- ActivityTest/app/src/main/java/com/example/administrator/activitytest/FirstActivity.java
+++ ActivityTest/app/src/main/java/com/example/administrator/activitytest/FirstActivity.java
@@ -1,12 +1,37 @@
 package com.example.administrator.activitytest;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(this, "This is add", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "This is remove", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +42,26 @@ public class FirstActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(FirstActivity.this,"You clicked it",Toast.LENGTH_SHORT).show();
+//                Intent intent=new Intent("com.example.administrator.activitytest.ACTION_START");
+//                intent.addCategory("com.example.administrator.activitytest.MY_CATEGORY");
+//                startActivity(intent);
+                String data="Hello SecondActivity";
+                Intent intent=new Intent(FirstActivity.this,SecondActivity.class);
+                intent.putExtra("extra_data",data);
+                startActivity(intent);
+            }
+        });
+        Button button1_1=(Button)findViewById(R.id.button_1_1);
+        button1_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(FirstActivity.this,"You clicked it 1_1",Toast.LENGTH_SHORT).show();
+//                Intent intent1=new Intent(Intent.ACTION_VIEW);
+//                intent1.setData(Uri.parse("http://www.baidu.com"));
+//                startActivity(intent1);
+                Intent intent1=new Intent(Intent.ACTION_DIAL);
+                intent1.setData(Uri.parse("tel:10086"));
+                startActivity(intent1);
             }
         });
     }
---
+++ ActivityTest/app/src/main/java/com/example/administrator/activitytest/SecondActivity.java
@@ -0,0 +1,19 @@
+package com.example.administrator.activitytest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+        Intent intent=getIntent();
+        String data=intent.getStringExtra("extra_data");
+        Toast.makeText(SecondActivity.this,data, Toast.LENGTH_SHORT).show();
+    }
+}
